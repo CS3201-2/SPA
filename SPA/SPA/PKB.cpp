07ee7dfaf692c4e89aa63e83f999c7032eb9267f
ZHAO HANG <davidzhao-81@163.com>
2015-08-30 02:42:50 +0800

    PKB module setup

---
+++ SPA/SPA/PKB.cpp
@@ -0,0 +1,16 @@
+#include "PKB.h"
+#include "AST.h"
+#include "Modifies.h"
+#include "Uses.h"
+#include "ProcTable.h"
+#include "VarTable.h"
+#include <string>
+#include <list>
+
+using namespace std;
+
+PKB::PKB()
+{
+
+}
+
---
+++ SPA/SPA/PKB.h
@@ -0,0 +1,27 @@
+#include "AST.h"
+#include "Modifies.h"
+#include "Uses.h"
+#include "ProcTable.h"
+#include "VarTable.h"
+#include <string>
+#include <list>
+
+using namespace std;
+
+#ifndef PKB_H
+#define PKB_H
+
+class PKB
+{
+public:
+	PKB();
+
+private:
+	AST AST;
+	Modifies modifies;
+	Uses uses;
+	ProcTable procTable;
+	VarTable varTable;
+};
+
+#endif
