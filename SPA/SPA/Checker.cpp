fbe47012bd7c7ffc00d09a6d3938b3613ff31bf7
ZHAO HANG <davidzhao-81@163.com>
2015-08-30 02:42:32 +0800

    Checker for Syntax setup.

---
+++ SPA/SPA/Checker.cpp
@@ -0,0 +1,17 @@
+#include "Checker.h"
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+typedef list<string> StringList;
+Checker::Checker() {
+
+}
+
+int Checker::checkSyntax( list<string> ) {
+	//TODO:
+	return 1;
+}
+
---
+++ SPA/SPA/Checker.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <list>
+
+using namespace std;
+
+#ifndef Checker_H
+#define Checker_H
+
+class Checker
+{
+public:
+	Checker();
+
+	int checkSyntax( list<string>);
+
+private:
+};
+
+#endif
