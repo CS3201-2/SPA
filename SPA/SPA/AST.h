2cc14d39922d697500ff763f32d49d8cf35f26b9
luckyxkw <cocolonel.x@gmail.com>
2015-09-01 15:55:25 +0800

    combine AST and ASTController

--- SPA/SPA/AST.h
+++ SPA/SPA/AST.h
@@ -11,7 +11,8 @@ class AST
 	ASTNode _root;
 
 public:
-	AST();
+	AST(ASTNode);
+	void updateAST(string);
 	list<ASTNode> getChildren(ASTNode);
 	ASTNode getParent(ASTNode);
 	list<ASTNode> getSiblings(ASTNode);
