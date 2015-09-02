f26000950810d551a33014233866ea29bf67171e
Yeo Kangwei <yeokangwei@hotmail.com>
2015-09-02 12:31:06 +0800

    Updated QueryController.cpp and QueryController.h

---
+++ SPA/SPA/QueryController.cpp
@@ -0,0 +1,6 @@
+#include "QueryController.h"
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
--- SPA/SPA/QueryController.h
+++ SPA/SPA/QueryController.h
@@ -1,1 +1,17 @@
-#pragma once
+#include <string>
+#include <list>
+
+using namespace std;
+
+#ifndef QueryController_H
+#define QueryController_H
+
+class QueryController
+{
+public:
+	QueryController(list<string>);
+
+private:
+};
+
+#endif
