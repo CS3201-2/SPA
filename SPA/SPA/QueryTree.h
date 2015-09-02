e51f34004b45144c33313213abc17011ef8b2871
Edward <angertrail@hotmail.com>
2015-09-02 13:36:17 +0800

    Added data structures (private)

--- SPA/SPA/QueryTree.h
+++ SPA/SPA/QueryTree.h
@@ -6,5 +6,17 @@ using namespace std;
 #pragma once
 
 class QueryTree {
+private:
+	vector<vector<string>> suchThatTree;
+	vector<vector<string>> patternTree;
+	vector<vector<string>> withTree;
+	vector<vector<string>> selectTree;
 
+public:
+	QueryTree();
+
+	void insertSuchThat(string rel, string val1, string val1_type, string val2, string val2_type);
+	void insertPattern(string synonym, string synonymType, string pattern1, string pattern1Type, 
+						string pattern2, string pattern2Type);
+	
 };
