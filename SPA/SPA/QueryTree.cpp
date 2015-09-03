548faa50adaa6171b0b7563d69edd2a69e1f6a23
Edward <angertrail@hotmail.com>
2015-09-03 10:33:53 +0800

    Added Pattern tree interface and code

--- SPA/SPA/QueryTree.cpp
+++ SPA/SPA/QueryTree.cpp
@@ -13,3 +13,14 @@ void QueryTree::insertSuchThat(string rel, string arg1, string arg1Type, string 
 	tempVector.push_back(arg2Type);
 	suchThatTree.push_back(tempVector);
 }
+
+void QueryTree::insertPattern(string syn, string synType, string ptrn1, string ptrn1Type, string ptrn2, string ptrn2Type) {
+	vector<string> tempVector;
+	tempVector.push_back(syn);
+	tempVector.push_back(synType);
+	tempVector.push_back(ptrn1);
+	tempVector.push_back(ptrn1Type);
+	tempVector.push_back(ptrn2);
+	tempVector.push_back(ptrn2Type);
+	patternTree.push_back(tempVector);
+}
--- SPA/SPA/QueryTree.h
+++ SPA/SPA/QueryTree.h
@@ -16,5 +16,5 @@ public:
 	QueryTree();
 
 	void insertSuchThat(string rel, string arg1, string arg1_type, string arg2, string arg2_type);
-	
+	void insertPattern(string syn, string synType, string ptrn1, string ptrn1Type, string ptrn2, string ptrn2Type);
 };
