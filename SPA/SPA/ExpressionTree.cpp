37b95f83503c4590b2f8d7d2aafe757f55560b2c
Kangwei Xiao <cocolonel.x@gmail.com>
2015-09-03 21:40:07 +0800

    update expressionTree

--- SPA/SPA/ExpressionTree.cpp
+++ SPA/SPA/ExpressionTree.cpp
@@ -21,16 +21,7 @@ string ExpressionTree::getSuffix(string str)
 {
 	unordered_map<char, int> inStack;
 	unordered_map<char, int> outStack;
-	inStack.emplace('1', 0);	outStack.emplace('1', 0);
-	inStack.emplace('2', 0);	outStack.emplace('2', 0);
-	inStack.emplace('3', 0);	outStack.emplace('3', 0);
-	inStack.emplace('4', 0);	outStack.emplace('4', 0);
-	inStack.emplace('5', 0);	outStack.emplace('5', 0);
-	inStack.emplace('6', 0);	outStack.emplace('6', 0);
-	inStack.emplace('7', 0);	outStack.emplace('7', 0);
-	inStack.emplace('8', 0);	outStack.emplace('8', 0);
-	inStack.emplace('9', 0);	outStack.emplace('9', 0);
-	inStack.emplace('0', 0);	outStack.emplace('0', 0);
+
 	inStack.emplace('(', 1);	outStack.emplace('(', 6);
 	inStack.emplace('+', 3);	outStack.emplace('+', 2);
 	inStack.emplace('-', 3);	outStack.emplace('-', 2);
