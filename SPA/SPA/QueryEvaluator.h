aacd0dfc3608dc8a7aa63b5439b3832405eb9d73
unknown <a0111683@A0111683.stu.nus.edu.sg>
2015-09-07 00:37:56 +0800

    Updated functionality for Modifies and Uses

--- SPA/SPA/QueryEvaluator.h
+++ SPA/SPA/QueryEvaluator.h
@@ -4,26 +4,29 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include "PKB.h"
 
 using namespace std; 
 
 class QueryEvaluator {
 private:
-
-	vector<string> getSuchThatClause(int index);
-	vector<string> getPatternClause(int index);
-	vector<string> getVarDeclaration(int index);
-	vector<string> combineResult(vector<string> suchThatResult, vector<string> patternResult);
-	string processSuchThatClause(vector<string> tempString);
-	string processPatternClause(vector<string> tempString);
-	vector<string> updateResult(vector<string> tempResult);
+	PKB pkb;
 	vector<vector<string>> result;
 	int suchThatTreeIndex = 0;
 	int patternTreeIndex = 0;
 
 public:
-	QueryEvaluator();
+	QueryEvaluator( PKB );
 	vector<vector<string>> getResult();
+	vector<string> getSuchThatClause(int index, QueryTree tree);
+	vector<string> getPatternClause(int index, QueryTree tree);
+	vector<string> getVarDeclaration(int index, QueryTree tree);
+	vector<string> combineResult(vector<string> suchThatResult, vector<string> patternResult);
+	string processSuchThatClause(vector<string> tempString);
+	string processPatternClause(vector<string> tempString);
+	vector<string> updateResult(vector<string> tempResult);
 };
 
 #endif
+
+QueryEvaluator(PKB my_pkb);
