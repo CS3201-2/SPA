53ede99628d02d557767f36257ce3c0c432e08d4
Edward <angertrail@hotmail.com>
2015-09-08 15:03:40 +0800

    Added functions for select tree

--- SPA/SPA/QueryTree.h
+++ SPA/SPA/QueryTree.h
@@ -12,6 +12,7 @@ private:
 	vector<vector<string>> patternTree;
 	vector<vector<string>> withTree;
 	vector<vector<string>> variableTree;
+	vector<vector<string>> selectTree;
 
 public:
 	QueryTree();
@@ -24,8 +25,11 @@ public:
 	void insertPattern(string syn, string synType, vector<string> arrPtrn, vector<string> ptrnType);
 	//For adding variables eg. assign a, => insertVariable(a, assign)
 	void insertVariable(string variable, string variableType);
+	//For adding variables under the select category
+	void insertSelect(string var, string varType);
 
 	//Method not yet implemented. For accepting relationships under the "with" clause. aka insertWith
+	// and ifPattern type. aka insertIfPattern
 
 	//Getting the number of relationships that fall under the such that clause
 	int getSuchThatSize();
@@ -33,6 +37,8 @@ public:
 	int getPatternSize();
 	//Getting the number of variables
 	int getVariableSize();
+	//Getting the number of variables under select
+	int getSelectSize();
 
 	//Obtaining the information of specific relationships that fall under the respective clauses
 	//eg. To see the first relationship under the such that tree, we call getSuchThatQuery(0) and assign it to a vector
@@ -40,4 +46,5 @@ public:
 	vector<string> getSuchThatQuery(int queryPos);
 	vector<string> getPatternQuery(int queryPos);
 	vector<string> getVariableQuery(int queryPos);
+	vector<string> getSelectQuery(int queryPos);
 };
