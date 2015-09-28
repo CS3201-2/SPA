d311a065803680bb7bfe3cd930e3443efa5added
Ma Cong <vincentm1991@gmail.com>
2015-09-28 19:16:12 +0800

    add all APIs into header for PKB

--- SPA/SPA/PKB.h
+++ SPA/SPA/PKB.h
@@ -3,6 +3,7 @@
 #include "Uses.h"
 #include "Follows.h"
 #include "Parent.h"
+#include "Calls.h"
 #include "ProcTable.h"
 #include "VarTable.h"
 #include "Pattern.h"
@@ -25,13 +26,114 @@ public:
 	Uses& getUses(void);
 	Parent& getParent(void);
 	Follows& getFollows(void);
+	Calls& getCalls();
 	AST& getAST(void);
 	Pattern& getPattern();
 	void addWhileList(int);
 	void addAssignList(int);
-	list<int> getWhileList(void);
-	list<int> getAssignList(void);
 	void setAST(AST);
+
+	//new APIs
+	//sort and unify function for Modifies, Uses, FollowsStar, CallsStar, Parent, ParentStar
+	//general
+	bool isValidStmtNo(int);
+	void addWhileToList(int);
+	void addAssignToList(int);
+	void addCallToList(int);
+	void addIfToList(int);
+	list<int> getWhileList();
+	list<int> getAssignList();
+	list<int> getCallList();
+	list<int> getIfList();
+	
+	//varTable
+	int insertVar(string);
+	int getVarID(string); 	//return 0 for invalid varName input
+	string getVarName(int);
+	void printVarTable();
+
+	//procTable
+	int insertProc(string);
+	int getProcID(string); 	//return 0 for invalid procName input
+	string getProcName(int);
+	void printProcTable();
+	
+	//Modifies
+	void setModifies(); // input parameter to be decided later
+	list<int> getModifiesFirst(int);
+	list<int> getModifiesSecond(int);
+	bool isModifiesValid(int, int);
+	void printAllModifies();
+
+	//Uses
+	void setUses(); //input parameter to be decided later
+	list<int> getUsesFirst(int);
+	list<int> getUsesSecond(int);
+	bool isUsesValid(int, int);
+	void printAllUses();
+
+	//Pattern
+	void setPattern(int, string, string);
+	list<int> getAssignWithFirst(string);
+	list<int> getAssignWithSecond(string);
+	list<int> getAssignWithBoth(string, string);
+	list<int> getIfWithFirst(string);
+	list<int> getWhileWithFirst(string);
+	void printAllPattern();
+
+	//Follows
+	void setFollows(int, int);
+	int getFollowsFirst(int);
+	int getFollowsSecond(int);
+	bool isFollowsValid(int, int);
+	void printAllFollows();
+
+	//FollowsStar
+	void setFollowsStar(int, list<int>); 
+	list<int> getFollowsStarFirst(int);
+	list<int> getFollowsStarSecond(int);
+	bool isFollowsStarValid(int, int);
+	void printAllFollowsStar();
+
+	//Calls
+	void setCalls(int, int);
+	int getCallsFirst(int);
+	int getCallsSecond(int);
+	bool isCallsValid(int, int);
+	void printAllCalls();
+
+	//CallsStar
+	void setCallsStar(int, list<int>);
+	list<int> getCallsStarFirst(int);
+	list<int> getCallsStarSecond(int);
+	bool isCallsStarValid(int, int);
+	void printAllCallsStar();
+
+	//Parent
+	void setParent(int, list<int>); 
+	int getParentFirst(int);
+	list<int> getParentSecond(int);
+	bool isParentValid(int, int);
+	void printAllParent();
+
+	//ParentStar
+	void setParentStar(int, list<int>);
+	list<int> getParentStarFirst(int);
+	list<int> getParentStarSecond(int);
+	bool isParentStarValid(int, int);
+	void printAllParent();
+
+	//Next
+	void setNext(int, int);
+	list<int> getNextFirst(int);
+	list<int> getNextSecond(int);
+	bool isNextvalid(int, int);
+	void printAllNext();
+
+
+
+	//new APIs ends
+
 
 private:
 	//static PKB* _PKBInstance;
@@ -43,6 +145,7 @@ private:
 	Parent parent;
 	Pattern pattern;
 	AST ast;
+	Calls calls;
 	list<int> whileList;
 	list<int> assignList;
 };
