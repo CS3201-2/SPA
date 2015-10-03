eb2762ff46c21a252ddbf74636f358db8bb6f3ba
luckyxkw <cocolonel.x@gmail.com>
2015-10-03 15:41:27 +0800

    delete some rubbish

--- SPA/SPA/CFGNodeType.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-enum CFGNodeType
-{
-	normal, whileclause, thenclause, elseclause
-};
--- SPA/SPA/NodeTypeEnum.h
+++
@@ -1,9 +0,0 @@
-#ifndef NodeTypeEnum_H
-#define NodeTypeEnum_H
-
-enum NodeType {
-	program, procedure, statementLst, assign, call, whilestmt, ifstmt,
-	variable, constant, plus, times, minus, divide, default, elf
-};
-
-#endif
