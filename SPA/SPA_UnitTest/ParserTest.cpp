152e894ea130253b98011a2c5ed6c51e2b9bb5e6
Ma Cong <vincentm1991@gmail.com>
2015-10-03 16:44:09 +0800

    changed visual studio tester

--- SPA/SPA_UnitTest/ParserTest.cpp
+++ SPA/SPA_UnitTest/ParserTest.cpp
@@ -11,32 +11,6 @@ namespace SPA_UnitTest
 	TEST_CLASS(testParser)
 	{
 	public:
-		
-		TEST_METHOD(trim)
-		{
-			Parser parser;
-			
-			string expected = "akshdbdjhdfbasdakhsbadkshbdhfsadhvhj";
-
-			string str = "akshdbdjhdfb  asdakhsb \n adkshbdhf\nsadhvhj";
-
-			parser.trim(str);
-
-			Assert::AreEqual( expected, str );
-		}
-
-
-		TEST_METHOD(addNewLineString)
-		{
-			Parser parser;
-			string expected = "procedure seconde{@";
-			string str = "procedure seconde{";
-
-			parser.addNewLineString(str);
-
-			Assert::AreEqual(expected, str);
-
-		}
 
 	};
 }
