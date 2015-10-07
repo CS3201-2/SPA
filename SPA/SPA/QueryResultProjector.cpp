607142aeca750e305f5b1a4d853630f7e0c2a23b
luckyxkw <cocolonel.x@gmail.com>
2015-10-07 12:56:52 +0800

    start optimize merging table

--- SPA/SPA/QueryResultProjector.cpp
+++ SPA/SPA/QueryResultProjector.cpp
@@ -61,16 +61,13 @@ list<string> QueryResultProjector::getResult() {
 				++it;
 			}
 		}
-
 	}
-
 	return resultStringList;
 }
 
 void QueryResultProjector::mergeTable() {
 	vector<string> resultHeader;
 	vector<vector<int>> result;
-
 	for (list<ResultTable>::iterator it = _resultList.begin(); it != _resultList.end(); ++it) {
 		if ((*it).getIsWholeTrue() == notApplicable) {//-1
 			//temp data is empty, break immediately
@@ -108,6 +105,24 @@ void QueryResultProjector::mergeTable() {
 
 	_resultTable = ResultTable(_isWholeTrue, resultHeader, result);
 	//make the resultTable object
+}
+
+void QueryResultProjector::mergeTableNew() {
+	vector<string> resultHeader;
+	vector<vector<int>> result;
+	list<ResultTable> tempList = _resultList;
+	bool flag = filter(tempList);//filter out all 0 and 1, and return true if contains 0
+	if (flag) {
+		_isWholeTrue = 0;
+		resultHeader.clear();
+		result.clear();
+	}
+	else {
+		for (auto& x : tempList) {
+
+		}
+	}
+	_resultTable = ResultTable(_isWholeTrue, resultHeader, result);
 }
 
 void QueryResultProjector::createResultHeader(vector<string>& resultHeader, vector<string> header) {
@@ -192,4 +207,9 @@ int QueryResultProjector::getIndexOf(vector<string> header, string str) {
 			return i;
 		}
 	}
+}
+
+bool filter(list<ResultTable>& temp)
+{
+
 }
