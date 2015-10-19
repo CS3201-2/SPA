2589a816b770336ad6128b58eedbd7c690c71d3b
luckyxkw <cocolonel.x@gmail.com>
2015-10-19 21:20:37 +0800

    try to implement getAffectSecond

--- SPA/SPA/Affect.cpp
+++ SPA/SPA/Affect.cpp
@@ -19,18 +19,18 @@ bool Affect::isAffectValid(int first, int second)
 	if (!contains(varUsesIndex, varModifiesIndex))
 		return false;
 	queue<int> path;
-	vector<bool> visit;
-	visit.resize(_pkb->getStmtSize(), false);
+	vector<int> visit;
+	visit.resize(_pkb->getStmtSize(), 0);
+	visit[first] == -1;
 	path.push(first);
 	while (!path.empty())
 	{
-		//assume first != second first
 		int temp = path.front();
-		if (temp == second)
+		if (temp == second && visit[temp] != -1)
 		{
 			return true;
 		}
-		else if (visit[temp])
+		else if (visit[temp] == 1)
 		{
 			//have visited already
 			//skip
@@ -38,7 +38,7 @@ bool Affect::isAffectValid(int first, int second)
 		else if (isWhile(temp))
 		{
 			path.pop();
-			visit[temp] = true;
+			visit[temp] = 1;
 			//the next line is not effective, try to optimize
 			list<int> tempLst = _pkb->getParentStarSecond(temp);
 			assert(temp + 1 == tempLst.front());
@@ -63,6 +63,7 @@ bool Affect::isAffectValid(int first, int second)
 		{
 			//go to both statLst
 			path.pop();
+			visit[temp] = 1;
 			list<int> tempNext = _pkb->getNextSecond(temp);
 			for (auto& x : tempNext)
 			{
@@ -73,7 +74,7 @@ bool Affect::isAffectValid(int first, int second)
 		{
 			path.pop();
 			list<int> tempModifies = _pkb->getModifiesSecond(temp);
-			if (!contains(tempModifies, varModifiesIndex))
+			if (visit[temp] == -1 || !contains(tempModifies, varModifiesIndex))
 			{
 				list<int> tempNext = _pkb->getNextSecond(temp);
 				if (!tempNext.empty())
@@ -82,9 +83,84 @@ bool Affect::isAffectValid(int first, int second)
 					path.push(tempNext.front());
 				}
 			}
+			visit[temp] = 1;
 		}
 	}
 	return false;
+}
+
+list<int> Affect::getAffectSecond(int start)
+{
+	list<int> buffer;
+	if (!isAssignment(start))
+		return buffer;
+	int varModifiesIndex = _pkb->getModifiesSecond(start).front();
+	queue<int> path;
+	vector<int> visit;
+	visit.resize(_pkb->getStmtSize(), 0);
+	visit[start] == -1;
+	path.push(start);
+	while (!path.empty())
+	{
+		int temp = path.front();
+		if (visit[temp] == -1)
+		{
+			//initial statement
+			visit[temp] == 1;
+			list<int> tempNext = _pkb->getNextSecond(temp);
+			if (!tempNext.empty())
+			{
+				assert(tempNext.size() == 1);
+				path.push(tempNext.front());
+			}
+		}
+		else
+		{
+			//check whether uses
+			list<int> varUsesIndex = _pkb->getUsesSecond(temp);
+			if (contains(varUsesIndex, varModifiesIndex))
+			{
+				buffer.push_back(temp);
+			}
+			//check whether visit
+			if (visit[temp] == 1)
+			{
+				continue;
+			}
+			else
+			{
+				visit[temp] == 1;
+				//check whether is container
+				if (isIf(temp) || isWhile(temp))
+				{
+					list<int> tempNext = _pkb->getNextSecond(temp);
+					for (auto& x : tempNext)
+					{
+						path.push(x);
+					}
+				}
+				else
+				{
+					//check whether modifies
+					list<int> tempModifeis = _pkb->getModifiesSecond(temp);
+					if (!contains(tempModifeis, varModifiesIndex))
+					{
+						list<int> tempNext = _pkb->getNextSecond(temp);
+						if (!tempNext.empty())
+						{
+							assert(tempNext.size() == 1);
+							path.push(tempNext.front());
+						}
+					}
+					else
+					{
+						//this path cannot use anymore
+					}
+				}
+			}
+		}
+	}
+	return buffer;
 }
 
 Affect::~Affect()
