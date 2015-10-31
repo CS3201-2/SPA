eb4dab0efdc1b83b33488f3e1a90da8da41ccc10
Ma Cong <vincentm1991@gmail.com>
2015-10-31 15:01:48 +0800

    changed merging table to ignore irrelevant tables

--- SPA/SPA/QueryResultProjector.cpp
+++ SPA/SPA/QueryResultProjector.cpp
@@ -30,7 +30,9 @@ list<string> QueryResultProjector::getResult() {
 		return resultStringList;
 	}
 
-	_finalTable = mergeTables();
+	vector<int> mergingOrder = getMergingOrder();
+
+	_finalTable = mergeTables(mergingOrder);
 	_finalTable.logTable(-1);
 
 	resultStringList = extractResultFromMergedTable();	
@@ -39,12 +41,12 @@ list<string> QueryResultProjector::getResult() {
 }
 
 //assume there is at least one table in _tempTables
-ResultTable QueryResultProjector::mergeTables() {
+ResultTable QueryResultProjector::mergeTables(vector<int> mergingOrder) {
 	ResultTable finalTable, tempTable;
-	finalTable = _tempTables.at(0);
+	finalTable = _tempTables.at(mergingOrder.at(0));
 
-	for (size_t i = 1; i < _tempTables.size(); ++i) {
-		tempTable = _tempTables.at(i);
+	for (size_t i = 1; i < mergingOrder.size(); ++i) {
+		tempTable = _tempTables.at(mergingOrder.at(i));
 		if (finalTable.getTableSize() < tempTable.getTableSize()) {
 			finalTable = mergeTwoTables(finalTable, tempTable);
 		}
@@ -60,172 +62,12 @@ ResultTable QueryResultProjector::mergeTables() {
 	return finalTable;
 }
 
-/*void QueryResultProjector::trimTempTables() {
-	for (int i = 0; i < _twoVarTables.size(); ++i) {
-		vector<string> header = _twoVarTables.at(i).getHeader();
-
-		//1 underscore in two header, remove _ column,
-		if (header.size() == 2 && (header.at(0) == "_" || header.at(1) == "_")
-			&& header.at(0) != header.at(1)) {
-			int underscoreIndex;
-			if (header.at(0) == "_") {
-				underscoreIndex = 0;
-			}
-			else {
-				underscoreIndex = 1;
-			}
-
-			header.erase(header.begin() + underscoreIndex);
-
-			vector<vector<int>> content = _tempTables.at(i).getContent();
-			for (auto &i : content) {
-				i.erase(i.begin() + underscoreIndex);
-			}
-
-			ResultTable newTable = ResultTable(-1, header, content);
-		}
-	}
-
-	//underscore in header, remove the table
-	for (vector<ResultTable>::iterator it = _tempTables.begin(); it != _tempTables.end();) {
-		if ((*it).getHeader().at(0) == "_") {
-			_tempTables.erase(it++);
-		}
-		else {
-			++it;
-		}
-	}
-}*/
-
-/*
-list<string> QueryResultProjector::getResultOld() {
-	list<string> resultStringList;
-
-	if(_tempTables.empty() && _selectType.size() == 1 
-		&& _selectType[FIRST_TYPE] == TYPE_BOOL) {
-		resultStringList.push_back(RESULT_TRUE);
-		return resultStringList;
-	}
-	
-	trimTempTablesOld();
-	vector<int> mergingOrder = getMergingOrder();	
-
-	ResultTable finalTable = mergeTables(mergingOrder);
-	finalTable.logTable(-1);
-
-	if (finalTable.getTableSize() != 0) {
-		//assume no select <a, boolean, b> kinda thing
-		if (_selectType.size() == 1 && _selectType[FIRST_TYPE] == TYPE_BOOL) {
-			resultStringList.push_back(RESULT_TRUE);
-			return resultStringList;
-		}
-
-		vector<int> index = getIndexOfSelect(finalTable.getHeader());
-		vector<vector<int>> finalContent = finalTable.getContent();
-
-		for (int i = 0; i < finalContent.size(); ++i) {
-			string result = "";
-			for (int j = 0; j < index.size(); ++j) {
-				if (result != "") {
-					result += " ";
-				}
-
-				if (_selectType[j] == TYPE_VARIABLE) {
-					string varName = PKB::getPKBInstance()->getVarName(finalContent[i][j]);
-					result += varName;
-				}
-				else if(_selectType[j] == TYPE_PROCEDURE){
-					string procName = PKB::getPKBInstance()->getProcName(finalContent[i][j]);
-					result += procName;
-				}
-				else {
-					result += to_string(finalContent[i][j]);
-				}
-			}
-
-			resultStringList.push_back(result);
-		}
-
-	}
-	else {
-		if (_selectType[FIRST_TYPE] == TYPE_BOOL) {
-			resultStringList.push_back(RESULT_FALSE);
-			return resultStringList;
-		}
-	}
-
-	resultStringList.sort();
-	resultStringList.unique();
-
-	string str;
-	for (auto &x : resultStringList) {
-		str += x + " ";
-	}
-	SPALog::log(str);
-
-	return resultStringList;
-}
-*/
-
-/*
-void QueryResultProjector::trimTempTablesOld() {
-	for (int i = 0; i < _tempTables.size(); ++i) {
-		vector<string> header = _tempTables.at(i).getHeader();
-		//same header, remove 1 column
-		if (header.size() == 2 && header.at(0) == header.at(1) 
-			&& header.at(0) != "_") {
-			header.erase(header.begin());
-			vector<vector<int>> content = _tempTables.at(i).getContent();
-			for (auto &i : content) {
-				i.erase(i.begin());
-			}
-
-			ResultTable newTable = ResultTable(-1, header, content);
-			_tempTables[i] = newTable;
-		}
-
-		//1 underscore in two header, remove _ column,
-		if (header.size() == 2 && (header.at(0) == "_" || header.at(1) == "_")
-			&& header.at(0) != header.at(1)) {
-			int underscoreIndex;
-			if (header.at(0) == "_") {
-				underscoreIndex = 0;
-			}
-			else {
-				underscoreIndex = 1;
-			}
-
-			header.erase(header.begin() + underscoreIndex);
-
-			vector<vector<int>> content = _tempTables.at(i).getContent();
-			for (auto &i : content) {
-				i.erase(i.begin() + underscoreIndex);
-			}
-
-			ResultTable newTable = ResultTable(-1, header, content);
-		}
-	}
-
-	//underscore in header, remove the table
-	for (vector<ResultTable>::iterator it = _tempTables.begin(); it != _tempTables.end();) {
-		if ((*it).getHeader().at(0) == "_") {
-			_tempTables.erase(it++);
-		}
-		else {
-			++it;
-		}
-	}
-}
-*/
-
-/*
 vector<int> QueryResultProjector::getMergingOrder() {
 	map<int, vector<string>> headerMap;
 	vector<string> tempHeaderList;
 	
-	bool _isSelectBool = isSelectBool();
 	//if it is select Boolean, 
-	if (_isSelectBool) {
+	if (_selectType[FIRST_TYPE] == TYPE_BOOL) {
 		countHeader();
 		for (map<string, int>::iterator it = _headerCount.begin(); it != _headerCount.end(); ++it) {
 			if ((*it).second > 1) {
@@ -296,7 +138,7 @@ vector<int> QueryResultProjector::getMergingOrder() {
 		}
 	}
 
-	if (_isSelectBool) {
+	if (_selectType[FIRST_TYPE] == TYPE_BOOL) {
 		for (map<int, vector<string>>::iterator it = headerMap.begin(); it != headerMap.end(); ++it) {
 			mergingOrder.push_back((*it).first);
 		}
@@ -309,18 +151,7 @@ vector<int> QueryResultProjector::getMergingOrder() {
 
 	return mergingOrder;
 }
-*/
 
-//assume multiselect does not have boolean type
-bool QueryResultProjector::isSelectBoolType() {
-	if ((_select.size() == 1 && _select[0] != "BOOLEAN")
-		|| _select.size() > 1) {
-		return false;
-	}
-	return true;
-}
-
-/*
 void QueryResultProjector::countHeader() {
 	for (auto &i : _tempTables) {
 		vector<string> header = i.getHeader();
@@ -335,29 +166,6 @@ void QueryResultProjector::countHeader() {
 		}
 	}
 }
-*/
-/*
-ResultTable QueryResultProjector::mergeTables(vector<int> mergingOrder) {
-	ResultTable finalTable, tempTable;
-	finalTable = _tempTables.at(mergingOrder.at(0));
-
-	for (int i = 1; i < mergingOrder.size(); ++i) {
-		tempTable = _tempTables.at(mergingOrder.at(i));
-		if (finalTable.getTableSize() < tempTable.getTableSize()) {
-			finalTable = mergeTwoTables(finalTable, tempTable);
-		}
-		else {
-			finalTable = mergeTwoTables(tempTable, finalTable);
-		}
-
-		if (finalTable.getTableSize() == 0) {
-			return ResultTable();
-		}
-	}
-
-	return finalTable;
-}
-*/
 
 ResultTable QueryResultProjector::mergeTwoTables(ResultTable r1, ResultTable r2) {
 	//r1 is always the table to be hashed into the unordered map
