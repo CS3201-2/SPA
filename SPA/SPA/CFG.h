f7feb720d863d47d16edb285adf9305d404b2f4a
luckyxkw <cocolonel.x@gmail.com>
2015-11-10 21:05:49 +0800

    delete useless files and functions in CFG

--- SPA/SPA/CFG.h
+++ SPA/SPA/CFG.h
@@ -26,15 +26,10 @@ class CFG
 	vector<list<int>> _next;
 	vector<list<int>> _nextTable;
 	vector<list<int>> _beforeTable;
-	vector<list<int>> _nextTableWithDummy;
-	vector<list<int>> _beforeTableWithDummy;
-	vector<list<int>> _dummyForNext;
-	vector<list<int>> _dummyForBefore;
 
 	vector<StatementType> _typeTable;
 	unordered_map<int, int> _procMap;
 	vector<int> _procTable;
-	unordered_map<int, int> _dummyMap;
 	unordered_map<int, CFGNode*> _nodeMap;
 
 	list<Statement>::iterator _codeIterator;
@@ -45,29 +40,23 @@ class CFG
 	int _nodeIndex;
 	int _size;
 	int _currentProc;
-	int _currentDummyNode;
 	bool _procFirstStatementSet;
 public:
 	CFG();
 	int getFirstStatement(int);
 	int getProc(int);
+	StatementType getType(int);
+
 	void buildGraph(list<Statement>);
+
 	list<int> getNextFirst(int);
 	list<int> getNextSecond(int);
 	bool isNextValid(int, int);
+
 	list<int> getNextStarFirst(int);
 	list<int> getNextStarSecond(int);
 	bool isNextStarValid(int, int);
-	list<int> getNextDummyFirst(int);
-	list<int> getNextDummySecond(int);
-	StatementType getType(int);
-	void printGraph();
-	void printNextTable();
-	void printBeforeTable();
-	void printNextTableWithDummy();
-	void printDummyForNext();
-	void printBeforeTableWithDummy();
-	void printDummyForBefore();
+	
 	void logNext();
 	~CFG();
 private:
@@ -84,8 +73,6 @@ private:
 	void updateVector(int, int, vector<list<int>>&);
 	void storeNextTable();
 	void storeNext(int);
-	void storeNextTableWithDummy();
-	void storeNextDummy(int);
 	bool isContainer(Statement);
 	bool isIfStmt(Statement);
 	bool isWhileStmt(Statement);
--- SPA/UnitTest/CFGTest.cpp
+++ SPA/UnitTest/CFGTest.cpp
@@ -274,13 +274,5 @@ namespace UnitTest {
 
 			}
 		}
-		TEST_METHOD(testNextWithDummy)
-		{
-			CFG cfg = constructInputNoNested();
-			cfg.printNextTableWithDummy();
-			cfg.printDummyForNext();
-			cfg.printBeforeTableWithDummy();
-			cfg.printDummyForBefore();
-		}
 	};
 }
